65e4d26b232a608bf220af4d88940c311a3e45a1
Nuno Fachada <nuno.fachada@ulusofona.pt>
2020-04-09 14:43:34 +0100

    Adicionar exercícios resolvidos da aula 5

---
+++ Aula05/Exercicio6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyTestOfRandoms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aux;
+            int n, soma = 0;
+            Random rnd = new Random();
+
+            Console.Write("Insira nº de dados: ");
+            aux = Console.ReadLine();
+            n = int.Parse(aux);
+
+            for (int i = 0; i < n; i++)
+            {
+                soma += rnd.Next(1, 7);
+            }
+            Console.WriteLine($"Soma = {soma}");
+
+        }
+    }
+}
